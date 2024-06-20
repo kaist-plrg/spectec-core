a0d01a878640650b7d4c6a5fde5d1bb95e1a2cd0
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2024-06-20 23:38:42 +0900

    fix: v0.0 to 0.1

--- doc/doc/conf.py
+++ doc/doc/conf.py
@@ -26,7 +26,7 @@ draft = ' (Auto-generated Draft ' + date.today().strftime("%Y-%m-%d") + ')'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.0'
+version = u'0.1'
 # The full version, including alpha/beta/rc tags.
 release = version + draft
 
