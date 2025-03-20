49c7ee8c92017cdbf92459093d41aadc0c6b5b18
KunJeong <kunjeong99@naver.com>
2025-03-20 15:22:08 +0900

    fix: checks explicit casting legality only on outermost type - removes special pretty print handling for `CastE(_, SeqE)` as it is no longer needed - addresses `p4c/program/well-typed/issue3779.p4`

--- p4/lib/il/pp_to_el.ml
+++ p4/lib/il/pp_to_el.ml
@@ -260,20 +260,12 @@ and pp_expr' ?(level = 0) fmt expr' =
   | CastE { typ; expr } -> (
       match typ.it with
       | SetT _ -> F.fprintf fmt "%a" (pp_expr ~level:(level + 1)) expr
-      | SpecT ((_, _, ListT _), _typs) ->
-        F.fprintf fmt "((%a) (%a))/*SpecT*/"
+      | _ ->
+        F.fprintf fmt "((%a) (%a))"
           (pp_typ ~level:(level + 1))
           typ
           (pp_expr ~level:(level + 1))
-          expr
-      | _ ->
-        (match expr.it with
-        | SeqE _ -> F.fprintf fmt "%a" (pp_expr ~level:(level + 1)) expr
-        | _ -> F.fprintf fmt "((%a) (%a))"
-            (pp_typ ~level:(level + 1))
-            typ
-            (pp_expr ~level:(level + 1))
-            expr))
+          expr)
   | MaskE { expr_base; expr_mask } ->
       F.fprintf fmt "%a &&& %a"
         (pp_expr ~level:(level + 1))
