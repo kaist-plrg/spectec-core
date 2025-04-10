3fa567f70bd580e2a2a2709703b41619781a1782
KunJeong <kunjeong99@naver.com>
2025-04-10 22:36:12 +0900

    fix: substitute wildcards only for InstD and CallInstS

--- p4/lib/typing/postprocess.ml
+++ p4/lib/typing/postprocess.ml
@@ -7,6 +7,23 @@ let base_walker = Il.Walk_transform.walker
 
 type walker = Il.Walk_transform.walker
 
+let hide_tparams_constyp typ =
+  match typ.it with
+  | SpecT (tdp, typs) ->
+      let tdp =
+        let tparams, tparams_hidden, typ = tdp in
+        let theta =
+          tparams_hidden
+          |> List.map (fun tparam -> (tparam, Types.AnyT))
+          |> Domain.Dom.TIdMap.of_list
+        in
+        let typ = Runtime_type.Subst.subst_typ theta typ in
+        (tparams, [], typ)
+      in
+      let it = SpecT (tdp, typs) in
+      { typ with it }
+  | _ -> assert false
+
 let hide_tparams_mthd (walker : walker) mthd =
   match mthd.it with
   | ExternConsM { id; tparams_hidden; cparams; annos } ->
@@ -41,25 +58,24 @@ let hide_tparams_decl (walker : walker) decl =
         PackageTypeD { id; tparams; tparams_hidden = []; cparams; annos }
       in
       { decl with it }
+  | InstD { id; typ; var_inst; targs; targs_hidden; args; init; annos } ->
+    let typ = hide_tparams_constyp typ in
+    let it =
+      InstD { id; typ; var_inst; targs; targs_hidden; args; init; annos }
+    in
+    { decl with it }
   | _ -> base_walker.walk_decl walker decl
 
-let hide_tparams_typ (walker : walker) typ =
-  let walk_typ' = W.walk_it (walker.walk_typ walker) () in
-  match typ.it with
-  | SpecT (tdp, typs) ->
-      let tdp =
-        let tparams, tparams_hidden, typ = tdp in
-        let theta =
-          tparams_hidden
-          |> List.map (fun tparam -> (tparam, Types.AnyT))
-          |> Domain.Dom.TIdMap.of_list
-        in
-        let typ = Runtime_type.Subst.subst_typ theta typ in
-        (tparams, [], typ)
+let hide_tparams_stmt (walker : walker) stmt =
+  match stmt.it with
+  | CallInstS { typ; var_inst; targs; args } ->
+      let it =
+        let typ = hide_tparams_constyp typ in
+        CallInstS { typ; var_inst; targs; args }
       in
-      let it = SpecT (tdp, W.walk_list walk_typ' typs) in
-      { typ with it }
-  | _ -> base_walker.walk_typ walker typ
+      { stmt with it }
+  | _ -> base_walker.walk_stmt walker stmt
+
 
 let postprocess_program (program : Il.Ast.program) : Il.Ast.program =
   let hide_tparams =
@@ -67,7 +83,7 @@ let postprocess_program (program : Il.Ast.program) : Il.Ast.program =
       Il.Walk_transform.walker with
       walk_mthd = hide_tparams_mthd;
       walk_decl = hide_tparams_decl;
-      walk_typ = hide_tparams_typ;
+      walk_stmt = hide_tparams_stmt;
     }
   in
   hide_tparams.walk_program hide_tparams program
