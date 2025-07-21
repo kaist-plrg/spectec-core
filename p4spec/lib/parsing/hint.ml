bdee7c9396127631a8b0d3e7bf58aaf8d25615ea
Sehyuk Ahn <shyukahn@gmail.com>
2025-07-21 15:28:23 +0900

    feat: implemented pp with hints

---
+++ p4spec/lib/parsing/hint.ml
@@ -0,0 +1,43 @@
+open Il.Ast
+open Util.Source
+module SMap = Map.Make (String)
+
+type hmap = (nottyp * El.Ast.exp) list SMap.t
+
+let hintid = "print"
+
+(* Types *)
+
+let hints_of_typcase hmap (id : id) (typcase : typcase) =
+  let nottyp, hints = typcase in
+  match List.find_opt (fun hint -> hint.hintid.it = hintid) hints with
+  | Some hint ->
+      let hint = (nottyp, hint.hintexp) in
+      let add = function
+        | None -> Some [ hint ]
+        | Some l -> Some (l @ [ hint ])
+      in
+      SMap.update id.it add hmap
+  | None -> hmap
+
+let hints_of_typcases hmap id typcases =
+  List.fold_left
+    (fun hmap typcase -> hints_of_typcase hmap id typcase)
+    hmap typcases
+
+let hints_of_deftyp hmap id (deftyp : deftyp) =
+  match deftyp.it with
+  | VariantT typcases -> hints_of_typcases hmap id typcases
+  | _ -> hmap
+
+(* Definitions *)
+
+let hints_of_def hmap (def : def) =
+  match def.it with
+  | TypD (id, _, deftyp) -> hints_of_deftyp hmap id deftyp
+  | _ -> hmap
+
+(* Spec *)
+
+let hints_of_spec (spec : spec) : hmap =
+  List.fold_left (fun hmap def -> hints_of_def hmap def) SMap.empty spec
