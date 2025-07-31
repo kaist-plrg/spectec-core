16b1a7f427c4a1e67b1e53f0b9994eb5248e398a
Sehyuk Ahn <shyukahn@gmail.com>
2025-07-31 11:16:23 +0900

    fix: added type annotations for function parameters in `pp.ml` and `hint.ml` for clarity

--- p4spec/lib/parsing/hint.ml
+++ p4spec/lib/parsing/hint.ml
@@ -8,7 +8,7 @@ let hintid = "print"
 
 (* Types *)
 
-let hints_of_typcase hmap (id : id) (typcase : typcase) =
+let hints_of_typcase hmap (id : id) (typcase : typcase) : hmap =
   let nottyp, hints = typcase in
   match List.find_opt (fun hint -> hint.hintid.it = hintid) hints with
   | Some hint ->
@@ -20,19 +20,19 @@ let hints_of_typcase hmap (id : id) (typcase : typcase) =
       SMap.update id.it add hmap
   | None -> hmap
 
-let hints_of_typcases hmap id typcases =
+let hints_of_typcases hmap (id : id) (typcases : typcase list) : hmap =
   List.fold_left
     (fun hmap typcase -> hints_of_typcase hmap id typcase)
     hmap typcases
 
-let hints_of_deftyp hmap id (deftyp : deftyp) =
+let hints_of_deftyp hmap (id : id) (deftyp : deftyp) : hmap =
   match deftyp.it with
   | VariantT typcases -> hints_of_typcases hmap id typcases
   | _ -> hmap
 
 (* Definitions *)
 
-let hints_of_def hmap (def : def) =
+let hints_of_def hmap (def : def) : hmap =
   match def.it with
   | TypD (id, _, deftyp) -> hints_of_deftyp hmap id deftyp
   | _ -> hmap
