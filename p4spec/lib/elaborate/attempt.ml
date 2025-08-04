a72c081c7e60ea95b5c6fd0a60b2eee3a4a49d02
KunJeong <kunjeong99@naver.com>
2025-08-04 16:58:51 +0900

    fix: revive full subfailtrace printing

--- p4spec/lib/elaborate/attempt.ml
+++ p4spec/lib/elaborate/attempt.ml
@@ -1,25 +1,47 @@
 include Util.Attempt
 open Error
 open Util.Source
+open Util.Print
 
 (* Monadic interface *)
 
 let ( let* ) (attempt : 'a attempt) (f : 'a -> 'b) : 'b =
   match attempt with Ok a -> f a | Fail _ as fail -> fail
 
-let string_of_subfailtrace (subfailtrace : failtrace) : string =
-  let (Failtrace (_, msg, _subfailtraces)) = subfailtrace in
-  Format.asprintf " || because %s" msg
+let rec string_of_failtrace_noregion ?(level = 0) ~(depth : int) ~(bullet : string)
+    (failtrace : failtrace) : string =
+  let (Failtrace (_, msg, subfailtraces)) = failtrace in
+  let smsg =
+    if level < depth then ""
+    else
+      Format.asprintf "  %s%s %s\n"
+        (indent (level - depth))
+        bullet msg
+  in
+  Format.asprintf "%s%s" smsg
+    (string_of_failtraces_noregion ~level:(level + 1) ~depth subfailtraces)
+
+and string_of_failtraces_noregion ?(level = 0) ~(depth : int)
+    (failtraces : failtrace list) : string =
+  match failtraces with
+  | [] -> ""
+  | [ failtrace ] -> string_of_failtrace_noregion ~level ~depth ~bullet:"└─" failtrace
+  | failtraces ->
+      List.mapi
+        (fun idx failtrace ->
+          string_of_failtrace_noregion ~level ~depth
+            ~bullet:(string_of_int (idx + 1) ^ ".")
+            failtrace)
+        failtraces
+      |> String.concat ""
 
 let format_failtraces (failtraces : failtrace list) : string =
   let error_of_failtrace (failtrace : failtrace) :
       (region * string) =
     let (Failtrace (at, msg, subfailtraces)) = failtrace in
-    let string_subfailtraces = 
-        List.map string_of_subfailtrace subfailtraces
-        |> String.concat ""
+    let string_subfailtraces = string_of_failtraces_noregion ~depth:0 subfailtraces
     in
-    (at, msg ^ string_subfailtraces)
+    (at, msg ^ "\n" ^ string_subfailtraces)
   in
   let all_errors = List.map error_of_failtrace failtraces in
   let formatted_errors =
