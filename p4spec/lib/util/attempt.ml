2bca3d37ff5e39569151a0a2a0606046f14621f0
KunJeong <kunjeong99@naver.com>
2025-08-11 08:22:22 +0900

    feat: remove prefix and indent for root error

--- p4spec/lib/util/attempt.ml
+++ p4spec/lib/util/attempt.ml
@@ -53,10 +53,14 @@ let rec string_of_failtrace ?(indent = "") ?(level = 0)
     ~(region_parent : region) ~(is_last : bool) ~(depth : int)
     ~(bullet : string) (failtrace : failtrace) : string =
   let (Failtrace (region, msg, subfailtraces)) = failtrace in
+  let is_root = level = 0 in
   let smsg =
     if level < depth then ""
     else
-      let prefix = if is_last then "└── " else "├── " in
+      let prefix = 
+        if is_root then "" else
+        if is_last then "└── " else "├── "
+      in
       let indent_prefix = String.make 4 ' ' in
       Format.asprintf "%s%s%s%s%s\n" indent prefix
         (if region_parent = region || region = no_region then ""
@@ -64,7 +68,7 @@ let rec string_of_failtrace ?(indent = "") ?(level = 0)
         bullet msg
   in
   let region_parent = if region = no_region then region_parent else region in
-  let indent = if is_last then indent ^ "    " else indent ^ "│   " in
+  let indent = if is_root then indent else if is_last then indent ^ "    " else indent ^ "│   " in
   Format.asprintf "%s%s" smsg
     (string_of_failtraces ~indent ~level:(level + 1) ~region_parent ~depth
        subfailtraces)
