deafc4ee2368a55f6e1158d9ba8b0a1ae2f202ad
KunJeong <kunjeong99@naver.com>
2025-08-11 00:22:50 +0900

    format

--- p4spec/lib/util/attempt.ml
+++ p4spec/lib/util/attempt.ml
@@ -49,8 +49,9 @@ let compare_failtrace failtrace_l failtrace_r =
 
 (* Error with backfailtraces *)
 
-let rec string_of_failtrace ?(indent = "") ?(level = 0) ~(region_parent : region) ~(is_last : bool)
-    ~(depth : int) ~(bullet : string) (failtrace : failtrace) : string =
+let rec string_of_failtrace ?(indent = "") ?(level = 0)
+    ~(region_parent : region) ~(is_last : bool) ~(depth : int)
+    ~(bullet : string) (failtrace : failtrace) : string =
   let (Failtrace (region, msg, subfailtraces)) = failtrace in
   let smsg =
     if level < depth then ""
@@ -59,28 +60,28 @@ let rec string_of_failtrace ?(indent = "") ?(level = 0) ~(region_parent : region
       let indent_prefix = String.make 4 ' ' in
       Format.asprintf "%s%s%s%s%s\n" indent prefix
         (if region_parent = region || region = no_region then ""
-         else
-           string_of_region region ^ "\n"
-            ^ indent ^ indent_prefix) bullet
-        msg
+         else string_of_region region ^ "\n" ^ indent ^ indent_prefix)
+        bullet msg
   in
   let region_parent = if region = no_region then region_parent else region in
   let indent = if is_last then indent ^ "    " else indent ^ "│   " in
   Format.asprintf "%s%s" smsg
-    (string_of_failtraces ~indent ~level:(level+1) ~region_parent ~depth subfailtraces)
+    (string_of_failtraces ~indent ~level:(level + 1) ~region_parent ~depth
+       subfailtraces)
 
-and string_of_failtraces ?(indent = "") ?(level = 0) ~(region_parent : region) ~(depth : int)
-    (failtraces : failtrace list) : string =
+and string_of_failtraces ?(indent = "") ?(level = 0) ~(region_parent : region)
+    ~(depth : int) (failtraces : failtrace list) : string =
   match failtraces with
   | [] -> ""
   | [ failtrace ] ->
-      string_of_failtrace ~indent ~level ~region_parent ~is_last:true ~depth ~bullet:"" failtrace
+      string_of_failtrace ~indent ~level ~region_parent ~is_last:true ~depth
+        ~bullet:"" failtrace
   | failtraces ->
       List.mapi
         (fun idx failtrace ->
           let is_last = idx = List.length failtraces - 1 in
-          let bullet = string_of_int (idx + 1) ^ ". "
-          in
-          string_of_failtrace ~indent ~level ~region_parent ~is_last ~depth ~bullet failtrace)
+          let bullet = string_of_int (idx + 1) ^ ". " in
+          string_of_failtrace ~indent ~level ~region_parent ~is_last ~depth
+            ~bullet failtrace)
         failtraces
       |> String.concat ""
