4e7daf3b9d3d9f2eab0683d7e9dd189ebd1f6385
KunJeong <kunjeong99@naver.com>
2025-09-06 17:14:58 +0900

    fix: elaboration in all lowercase

--- spectec/bin/main.ml
+++ spectec/bin/main.ml
@@ -13,7 +13,7 @@ let elab_command =
        try
          let spec = List.concat_map Frontend.Parse.parse_file filenames in
          let spec_il = Elaborate.Elab.elab_spec spec in
-         Format.printf "%s\n" (Il.Print.string_of_spec spec_il);
+         Format.printf "%s\n" (Il.Print_debug.string_of_spec spec_il);
          ()
        with
        | ParseError (at, msg) -> Format.printf "%s\n" (string_of_error at msg)
