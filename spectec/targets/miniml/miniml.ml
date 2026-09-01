kaist-plrg/spectec-core

commit d284ae5f8d418ebb12bee396f718c9f9deeb8cf7

feat(targets/miniml): add executable target
Adds the Mini-ML specification, object-language parser, surface printer,
`Eval` task, and `miniml` command group. The initial corpus covers core
evaluation and expected failures.

The port folds source parser and printer corrections into the initial
target. It applies repository formatting and removes redundant source
comments and an unused metavariable.

Adapted from backtracking/spectecx.

Original-commit: https://github.com/backtracking/spectecx/commit/ce4249aac9a8
Original-commit: https://github.com/backtracking/spectecx/commit/9d6e0eccd2d6
Original-commit: https://github.com/backtracking/spectecx/commit/1f6c1e66899e
Original-commit: https://github.com/backtracking/spectecx/commit/1b7af5d046f4

spectec/targets/miniml/miniml.ml
@@ -1,0 +1,110 @@
+let collect_files_recursive ~suffix dir =
+  let rec gather acc path =
+    if Sys.file_exists path && Sys.is_directory path then (
+      let entries = Sys.readdir path in
+      Array.sort String.compare entries;
+      Array.fold_left
+        (fun acc name -> gather acc (Filename.concat path name))
+        acc entries)
+    else if Filename.check_suffix path suffix then path :: acc
+    else acc
+  in
+  if Sys.file_exists dir then gather [] dir |> List.rev else []
+
+let contains_substring s sub =
+  try
+    let _ = Str.search_forward (Str.regexp_string sub) s 0 in
+    true
+  with Not_found -> false
+
+module Target : Spectec.Target.S = struct
+  let name = "miniml"
+  let spec_dir = "spectec/specs/miniml"
+  let builtins : (string * Builtins.Define.t) list = []
+
+  let handler f =
+    let vid_counter = ref 0 in
+    let fresh_vid () =
+      let v = !vid_counter in
+      incr vid_counter;
+      v
+    in
+    Lang.Il.Value.GlobalVidProvider.set fresh_vid;
+    f ()
+
+  let is_impure_func _ = false
+  let is_impure_rel _ = false
+  let state_version = ref 0
+end
+
+type input = { filename : string; expect : Spectec.Task.expectation }
+
+let collect_with ~classify ?dir () =
+  match dir with
+  | None -> []
+  | Some test_dir ->
+      collect_files_recursive ~suffix:".ml" test_dir
+      |> List.map (fun filename -> { filename; expect = classify filename })
+
+let eval_classify filename =
+  if contains_substring filename "_error" then Spectec.Task.Negative
+  else Spectec.Task.Positive
+
+module Task_common = struct
+  module Target = Target
+
+  type nonrec input = input
+
+  let unparse = Unparse.unparse
+  let parse_string = Lexer.parse_string
+  let source ({ filename; _ } : input) = filename
+  let expectation ({ expect; _ } : input) = expect
+  let save_output _ _ = ()
+end
+
+module Eval = struct
+  include Task_common
+
+  let name = "evaluator"
+  let collect = collect_with ~classify:eval_classify
+
+  let parse_input ~spec:_ { filename; _ } =
+    Lexer.parse_file ~handler:Target.handler filename
+    |> Result.map (fun v -> ("Eval_expr", [ v ]))
+
+  let format_output = function
+    | [] -> "Eval succeeded (no output)"
+    | vs -> vs |> List.map Lang.Il.Print.string_of_value |> String.concat ", "
+  (* FIXME: rather use Unparse.unparse here? *)
+end
+
+let cli_flags =
+  let open Core.Command.Let_syntax in
+  let open Core.Command.Param in
+  let%map filename = flag "-p" (required string) ~doc:"FILE miniml file" in
+  { filename; expect = Spectec.Task.Positive }
+
+module Eval_cli : Cli.Task_cli.S = struct
+  module Task = Eval
+
+  let flags = cli_flags
+end
+
+module Cli : Cli.Target_cli.S = struct
+  module Target = Target
+
+  let name = Target.name
+
+  let command =
+    let target = (module Target : Spectec.Target.S) in
+    let module Subcommand = Cli.Subcommand in
+    Core.Command.group ~summary:"miniml commands"
+      [
+        Subcommand.make_task target ~name:"eval" ~summary:"Run miniml evaluator"
+          (module Eval_cli);
+        Subcommand.make_parse target ~name:"parse"
+          ~summary:"parse a minml program to an IL value"
+          (module Eval_cli);
+        Subcommand.make_checkpoint target ~name:"checkpoint";
+      ]
+end

spectec/targets/miniml/unparse.ml
@@ -1,0 +1,37 @@
+open Format
+open Lang.Il
+
+let flatten = Value.flatten_case_v
+
+let rec print_expr fmt e =
+  match flatten e with
+  | _, [ "Eapp" ], [ e1; e2 ] ->
+      fprintf fmt "@[%a@ %a@]" print_expr e1 print_atom e2
+  | _, [ "Efun" ], [ x; e ] ->
+      fprintf fmt "@[fun %s ->@ %a@]" (Value.get_text x) print_expr e
+  | _, [ "Elet" ], [ x; e1; e2 ] ->
+      fprintf fmt "@[let %s = %a in@ %a@]" (Value.get_text x) print_expr e1
+        print_expr e2
+  | _ -> print_atom fmt e
+
+and print_atom fmt e =
+  match flatten e with
+  | _, [ "Ecst" ], [ n ] ->
+      let n = Xl.Num.to_int (Value.get_num n) in
+      fprintf fmt "%s" (Bigint.to_string n)
+  | _, [ "Eop" ], [ o ] -> (
+      match flatten o with
+      | _, [ "ADD" ], _ -> fprintf fmt "+"
+      | _ -> failwith "unexpected operator")
+  | _, [ "Evar" ], [ x ] -> fprintf fmt "%s" (Value.get_text x)
+  | _, [ "Epair" ], [ e1; e2 ] ->
+      fprintf fmt "(@[%a,@ %a@])" print_expr e1 print_expr e2
+  | _, [ ("Eapp" | "Efun" | "Elet") ], _ -> fprintf fmt "(@[%a@])" print_expr e
+  | _ -> fprintf fmt "<print_expr: unexpected term>"
+
+let print_expr (e : Value.t) : string = kasprintf (fun s -> s) "%a" print_expr e
+
+let unparse ~spec:_ (values : Value.t list) : string =
+  match values with
+  | [ e ] -> print_expr e
+  | _ -> failwith "unparse expects a single expression"

spectec/bin/dune
@@ -11,6 +11,7 @@
   spectec.backend.splice
   spectec.targets.p4
   spectec.targets.impty
+  spectec.targets.miniml
   core
   core_unix
   core_unix.command_unix))

spectec/bin/main.ml
@@ -180,6 +180,7 @@ let splice_command =
 let command =
   let module P4 = Targets_p4.P4.Cli in
   let module Impty = Targets_impty.Impty.Cli in
+  let module Miniml = Targets_miniml.Miniml.Cli in
   Core.Command.group ~summary:"SpecTec command line tools"
     [
       ("unparse", unparse_command);
@@ -190,6 +191,7 @@ let command =
       ("splice", splice_command);
       (P4.name, P4.command);
       (Impty.name, Impty.command);
+      (Miniml.name, Miniml.command);
     ]
 
 let () = Command_unix.run ~version command

spectec/specs/miniml/spec.spectec
@@ -1,0 +1,112 @@
+;;--------------------
+;; Syntax
+
+syntax id =
+  text
+
+syntax op =
+  | ADD
+
+syntax expr =
+  | Ecst  int
+  | Eop   op
+  | Evar  id
+  | Eapp  expr expr
+  | Efun  id expr
+  | Elet  id expr expr
+  | Epair expr expr
+
+;;--------------------
+;; Substitution
+
+var n: int
+var x: id
+var y: id
+var e1: expr
+var e2: expr
+var e: expr
+var v: expr
+
+dec $subst(expr, id, expr) : expr
+  hint(prose_in "%0[%1 <- %2]")
+
+dec $subst_var(id, id, expr) : expr
+  hint(prose_in "%0[%1 <- %2]")
+
+dec $subst_fun(id, expr, id, expr) : expr
+  hint(prose_in "(Efun %0 %1)[%2 <- %3]")
+
+def $subst_var(y, x, v) = v
+  -- if x = y
+def $subst_var(y, x, v) = Evar y
+ -- otherwise
+
+def $subst_fun(y, e1, x, v) = Efun y e1
+  -- if x = y
+def $subst_fun(y, e1, x, v) = Efun y $subst(e1, x, v)
+  -- otherwise
+
+
+def $subst(Ecst n, x, v) = Ecst n
+def $subst(Eop op, x, v) = Eop op
+def $subst(Evar y, x, v) = $subst_var(y, x, v)
+def $subst(Eapp e1 e2, x, v) = Eapp $subst(e1, x, v) $subst(e2, x, v)
+def $subst(Efun y e1, x, v) = $subst_fun(y, e1,x, v)
+def $subst(Elet y e1 e2, x, v) = Elet y $subst(e1, x, v) e2
+  -- if x = y
+def $subst(Elet y e1 e2, x, v) = Elet y $subst(e1, x, v) $subst(e2, x, v)
+  -- otherwise
+def $subst(Epair e1 e2, x, v) = Epair $subst(e1, x, v) $subst(e2, x, v)
+
+;;--------------------
+;; Evaluation
+
+syntax value = expr
+
+relation Eval_expr:
+  expr ==> value
+  hint(input %0)
+  hint(prose_in "evaluating" %0)
+
+var o: op
+
+rule Eval_expr/cst:
+  Ecst n ==> Ecst n
+rule Eval_expr/op:
+  Eop o ==> Eop o
+rule Eval_expr/fun:
+  Efun x e ==> Efun x e
+
+var i: int
+var i1: int
+var i2: int
+var v1: expr
+var v2: expr
+
+rule Eval_expr/add:
+  Eapp e1 e2 ==> Ecst i
+  -- Eval_expr: e1 ==> Eop ADD
+  -- Eval_expr: e2 ==> Epair v1 v2
+  -- if v1 = Ecst i1
+  -- if v2 = Ecst i2
+  -- if i = $(i1 + i2)
+
+rule Eval_expr/pair:
+  Epair e1 e2 ==> Epair v1 v2
+  -- Eval_expr: e1 ==> v1
+  -- Eval_expr: e2 ==> v2
+
+rule Eval_expr/let:
+  Elet x e1 e2 ==> v2
+  -- Eval_expr: e1 ==> v1
+  -- if $subst(e2, x, v1) = e
+  -- Eval_expr: e ==> v2
+
+var body: expr
+
+rule Eval_expr/beta:
+  Eapp e1 e2 ==> v
+  -- Eval_expr: e1 ==> Efun x body
+  -- Eval_expr: e2 ==> v2
+  -- if $subst(body, x, v2) = e
+  -- Eval_expr: e ==> v

spectec/targets/miniml/dune
@@ -1,0 +1,22 @@
+(ocamllex
+ (modules lexer))
+
+(menhir
+ (flags -v --fixed-exception)
+ (modules parse))
+
+(library
+ (name targets_miniml)
+ (public_name spectec.targets.miniml)
+ (libraries
+  spectec
+  spectec.cli
+  spectec.common
+  spectec.grammar
+  spectec.lang
+  spectec.lang.il
+  spectec.interp.builtins
+  core
+  str)
+ (preprocess
+  (pps ppx_let)))

spectec/targets/miniml/error.ml
@@ -1,0 +1,5 @@
+open Common.Source
+
+exception MinimlParseError of region * string
+
+let error (at : region) (msg : string) = raise (MinimlParseError (at, msg))

spectec/targets/miniml/lexer.mll
@@ -1,0 +1,67 @@
+{
+  open Lexing
+  open Parse
+  open Common.Source
+
+  let pos p =
+    { file = p.pos_fname; line = p.pos_lnum; column = p.pos_cnum }
+  let region lb =
+    { left = pos (lexeme_start_p lb); right = pos (lexeme_end_p lb) }
+}
+
+let digit = ['0'-'9']
+let char = ['a'-'z' '_' 'A'-'Z']
+
+rule token = parse
+| [' ' '\t' '\r']+
+  { token lexbuf }
+| '\n'
+  { new_line lexbuf; token lexbuf }
+| digit+ as s
+  { CST (Bigint.of_string s) }
+| "let"
+  { LET }
+| "in"
+  { IN }
+| "fun"
+  { FUN }
+| char+ as s
+  { ID s }
+| '+'
+  { ADD }
+| '('
+  { LPAR }
+| ')'
+  { RPAR }
+| ','
+  { COMMA }
+| '='
+  { EQUAL }
+| "->"
+  { ARROW }
+| _ as c
+  { Error.error (region lexbuf) (Printf.sprintf "lexical error: %c" c) }
+| eof
+  { EOF }
+
+{
+
+  let parse ~filename lb =
+    set_filename lb filename;
+    try Ok (Parse.prog token lb) with
+    | Parsing.Parse_error ->
+      Error (Spectec.Error.TaskParseError (region lb, "syntax error"))
+    | Error.MinimlParseError (at, msg) ->
+      Error (Spectec.Error.TaskParseError (at, msg))
+
+  let parse_file ~handler filename =
+    handler @@ fun () ->
+    let c = open_in filename in
+    let lb = from_channel c in
+    let r = parse ~filename lb in close_in c; r
+
+  let parse_string ~spec:_ ~filename content =
+    let lb = from_string content in
+    Result.bind (parse ~filename lb) (fun e -> Ok [e])
+
+}

commit 6a536ec69d0de87c8cf2f0472b398798fd0c9b4e

fix(targets/miniml): use Mini-ML surface conventions
The evaluator exposed IL constructors. Its help text used inconsistent
language names and contained a typo. The printer also changed some
applications during round-trip parsing.

`Eval.format_output` renders a single result with `Unparse.print_expr`.
The command descriptions use `Mini-ML`, and applications parenthesize a
compound left operand when required to preserve the parsed tree.

Fixes: d284ae5f8d41 ("feat(targets/miniml): add executable target")

spectec/targets/miniml/miniml.ml
@@ -73,15 +73,15 @@ module Eval = struct
     |> Result.map (fun v -> ("Eval_expr", [ v ]))
 
   let format_output = function
+    | [ v ] -> Unparse.print_expr v
     | [] -> "Eval succeeded (no output)"
     | vs -> vs |> List.map Lang.Il.Print.string_of_value |> String.concat ", "
-  (* FIXME: rather use Unparse.unparse here? *)
 end
 
 let cli_flags =
   let open Core.Command.Let_syntax in
   let open Core.Command.Param in
-  let%map filename = flag "-p" (required string) ~doc:"FILE miniml file" in
+  let%map filename = flag "-p" (required string) ~doc:"FILE Mini-ML file" in
   { filename; expect = Spectec.Task.Positive }
 
 module Eval_cli : Cli.Task_cli.S = struct
@@ -98,12 +98,13 @@ module Cli : Cli.Target_cli.S = struct
   let command =
     let target = (module Target : Spectec.Target.S) in
     let module Subcommand = Cli.Subcommand in
-    Core.Command.group ~summary:"miniml commands"
+    Core.Command.group ~summary:"Mini-ML commands"
       [
-        Subcommand.make_task target ~name:"eval" ~summary:"Run miniml evaluator"
+        Subcommand.make_task target ~name:"eval"
+          ~summary:"Run Mini-ML evaluator"
           (module Eval_cli);
         Subcommand.make_parse target ~name:"parse"
-          ~summary:"parse a minml program to an IL value"
+          ~summary:"Parse a Mini-ML program to an IL value"
           (module Eval_cli);
         Subcommand.make_checkpoint target ~name:"checkpoint";
       ]

spectec/targets/miniml/unparse.ml
@@ -6,7 +6,7 @@ let flatten = Value.flatten_case_v
 let rec print_expr fmt e =
   match flatten e with
   | _, [ "Eapp" ], [ e1; e2 ] ->
-      fprintf fmt "@[%a@ %a@]" print_expr e1 print_atom e2
+      fprintf fmt "@[%a@ %a@]" print_atom e1 print_atom e2
   | _, [ "Efun" ], [ x; e ] ->
       fprintf fmt "@[fun %s ->@ %a@]" (Value.get_text x) print_expr e
   | _, [ "Elet" ], [ x; e1; e2 ] ->